8cdd30cd557d8a821de2592540c18c7b16bcf79b
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-05 22:25:06 +0700

    day 1

--- employee.py
+++ employee.py
@@ -1,6 +1,6 @@
 class Employee:
-    def __init__(self):
-        self.age = "Employee age is **"
+    def __init__(self, age="**"):
+        self.age = f"Employee age is {age}"
 
     def EmployeeID(self):
         print("This is just employee unique identity")
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from employee import Employee
 
 
-emp = Employee()
+if __name__ == "__main__":
+    emp = Employee(age=30)
 
-print(emp.age)
-emp.EmployeeID()
+    print(emp.age)
+    emp.EmployeeID()
+
+    emp.age = 35
+    print(emp.age)
