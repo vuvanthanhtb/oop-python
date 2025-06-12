4f46c81310ef7059d0254703cbbde4d03f25b77b
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-12 19:47:06 +0700

    day 3

---
+++ 4.Building Blocks/my_class_2.py
@@ -0,0 +1,10 @@
+class MyClass:
+    var = 9
+    def firstM(self):
+        print("Hello, world")
+
+
+obj = MyClass()
+print(obj.var)
+obj.firstM()
+
---
+++ 4.Building Blocks/my_class_3.py
@@ -0,0 +1,10 @@
+class MyClass:
+    def firstM(self):
+        print("Hello, world")
+        print(self)
+
+
+obj = MyClass()
+obj.firstM()
+
+print(obj)
